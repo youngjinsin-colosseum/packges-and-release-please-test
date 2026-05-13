c86fdc8330ef9385ea49c08524e8920c2ba40d0e
신영진 <sinbom@colosseum.kr>
2026-05-13 11:45:24 +0900

    fix: publish 테스트

---
+++ build-logic/src/main/kotlin/sample.publishing-conventions.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    `maven-publish`
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/youngjinsin-colosseum/packges-and-release-please-test")
+            credentials {
+                username = providers.gradleProperty("gpr.user").orNull ?: System.getenv("GPR_USER")
+                password = providers.gradleProperty("gpr.token").orNull ?: System.getenv("GPR_TOKEN")
+            }
+        }
+    }
+}
--- sample-domain/build.gradle.kts
+++ sample-domain/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("sample.library-conventions")
+    id("sample.publishing-conventions")
 }
 
 description = "Sample 1.0 Domain Module"
